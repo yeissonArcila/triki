8aee01cf30f750acd711813ecb61cc08cc4d73da
yeisson <arcilavasquez6@gmail.com>
2022-08-08 09:31:15 -0500

    inicialisamos las variables

---
+++ scrips/app.js
@@ -0,0 +1,19 @@
+let celdas=["","","","","","","","",""]
+
+let jugadorActual="X"
+let resultado=document.querySelector(".result")
+
+let botones=document.querySelectorAll(".btn")
+
+let condiciones=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [0,4,8],
+
+
+]
